909c082eddd56e78629c41546b404158c5c47068
Shravan Jambukesan <shravan.j97@gmail.com>
2020-11-05 21:23:51 -0600

    Added entities for MarketHolidays table and QueuedOrders table

---
+++ PMCommonEntities/Models/MarketHolidays.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMCommonEntities.Models
+{
+    public class MarketHolidays
+    {
+        public int Id { get; set; }
+        public DateTime HolidayDate { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ PMCommonEntities/Models/QueuedOrders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMCommonEntities.Models
+{
+    public class QueuedOrders
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Symbol { get; set; }
+        public string OrderType { get; set; }
+        public int Quantity { get; set; }
+    }
+}
